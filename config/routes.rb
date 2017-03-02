f072104839346f62818f26c573ab3eba3c6ce12c
Brian Dorton <dorton@gmail.com>
2017-03-01 21:36:04 -0600

    updates root and adds the fun

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :players
   get 'home/index'
-  root 'home#index'
+  root 'games#new'
   resources :games
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
